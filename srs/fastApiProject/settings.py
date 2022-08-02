d391b94bbb0dc4e466c1f1b84b3e74b5f186d453
tonn1oo <ant.vlasov.87@gmail.com>
2022-08-03 02:41:48 +0300

    added auth

--- srs/fastApiProject/settings.py
+++ srs/fastApiProject/settings.py
@@ -6,10 +6,14 @@ class Settings(BaseSettings):
     server_port: int = 8000
     database_url: str = 'sqlite:///./database.sqlite3'
 
+    jwt_secret: str
+    jwt_algorithm: str = 'HS256'
+    jwt_expiration: int = 3600
+
 
 settings = Settings(
     _env_file='.env',
-    _env_file_encoding='utf-8'
+    _env_file_encoding='utf-8',
 )
 
 
